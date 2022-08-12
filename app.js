52bf24b9c448f7c290a136f31fd1d31df91dd765
syed muhammad bilal <syedmuhammadbilal638@gmail.com>
2022-08-12 18:51:34 +0500

    new

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Function called whenever user tab on any box
 function myfunc() {
-
+//some of important
 	// Setting DOM to all boxes or input field
 	var b1, b2, b3, b4, b5, b6, b7, b8, b9;
 	b1 = document.getElementById("b1").value;
